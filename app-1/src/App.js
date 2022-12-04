b123aca3de4a4c6dff796e38abc907e914ee73a9
Vishakha More <vish.wishtree@gmail.com>
2022-12-04 19:33:01 +0530

    initial commit

---
+++ app-1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ExpenseAll from './components/Expenses/ExpenseAll';
+import NewExpense from './components/NewExpense/NewExpense';
+
+function App() {
+
+  return (
+    <>
+      <NewExpense />
+      <ExpenseAll />
+    </>
+  );
+}
+
+export default App;
